69266d1e7d3dc190b218a825e747136b4fb65cbb
NSEVC <425237677@qq.com>
2017-09-11 20:02:09 +0800

    a better method for q2addTwoNumbers

--- q2/2AddTwoNumbers.cpp
+++ q2/2AddTwoNumbers.cpp
@@ -6,6 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/*
 #include <iostream>
 
 using namespace std;
@@ -101,7 +102,86 @@ int main(){
     }
 
     cout << endl;
-    /**/
 
     return 0;
 }
+*/
+
+//==============================================================================
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(NULL) {}
+};
+
+class Solution{
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+        int sum = 0;
+        ListNode *output = NULL;
+        ListNode *tail = NULL;
+        while(l1 != NULL || l2 != NULL){
+            if(l1 != NULL){
+                sum = sum + l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != NULL){
+                sum = sum + l2->val;
+                l2 = l2->next;
+            }
+            ListNode *p = new ListNode(sum%10);
+            if(output == NULL){
+                output = p;
+                tail = p;
+            }
+            else{
+                tail->next = p;
+                tail = p;
+            }
+
+            sum = sum / 10; //carry
+        }
+        if(sum != 0){
+            ListNode *p = new ListNode(sum);
+            tail->next = p;
+        }
+        return output;
+    }
+};
+
+void push(ListNode* node, int value){
+    ListNode *p = new ListNode(value);
+    ListNode *head = node;
+    while(head->next != NULL){
+        head = head->next;
+    }
+    head->next = p;
+}
+
+int main(){
+    Solution s;
+    ListNode *l1 = new ListNode(3);
+    push(l1,2);
+    push(l1,9);
+
+    ListNode *l2 = new ListNode(3);
+    push(l2,3);
+    push(l2,2);
+
+    ListNode *output = s.addTwoNumbers(l1, l2);
+    while(output != NULL){
+        cout << output->val << " ";
+        output = output->next;
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+
